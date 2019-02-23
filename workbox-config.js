c0b37b475d7cb898f8b130d0bcba287b3ff99a11
James Williams <james@jamesrwilliams.co.uk>
2019-02-22 22:18:55 -0500

    feat: PWA assets and more config for WorkBox

---
+++ workbox-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "globDirectory": "public/",
+  "globPatterns": [
+    "**/*.{png,ico,json,jpg,js,txt,css}",
+    "index.html",
+    "offline/index.html",
+    "404.html"
+  ],
+  "swDest": "static/sw.js",
+  "swSrc": "archetypes/sw.js"
+};
